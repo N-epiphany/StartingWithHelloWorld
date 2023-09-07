b7a669010ea3bff46601b4ac7b09d80821755ee2
Bhavya Srivastava <83921370+N-epiphany@users.noreply.github.com>
2023-09-07 22:41:29 +0530

    Time: 246 ms (45.84%), Space: 10.3 MB (61.33%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+          //we are creating an vector to store values
+            vector<int> result;
+            //iterations
+              for(int i=0;i<nums.size();i++){
+               
+                  int get = target - nums[i];    // the value we need to find
+                  // cout<<"\n \n THIS IS ITERATION"<<i<<endl;
+
+                  // cout<<"target : "<<target<<" : ";
+                  // cout<<"get : "<<get<<endl;
+
+                  for(int j=i+1;j<nums.size();j++){
+
+                      // cout<<"Inside sec loop get:"<<get;
+                      // cout<<" nums[j]:"<<j <<"value"<<nums[j]<<endl;
+                      if(nums[j]==get){
+                        // cout<<"we are pushing back"<<i<<":"<<j<<endl;
+                        result.push_back(i);
+                        result.push_back(j);
+                      }
+                  }                
+            }
+            return result;
+    }
+};
