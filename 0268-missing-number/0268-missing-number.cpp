5acd45a3219e3fddc6048c3d9cdc296fddd5b555
Bhavya Srivastava <83921370+N-epiphany@users.noreply.github.com>
2023-09-11 22:33:41 +0530

    Time: 11 ms (90.18%), Space: 17.9 MB (47.2%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        //since its ranging from 0 to n 
         int n= nums.size()+1;
-        // cout<<n<<";";
+        //since its ranging from 0 to n 
         int targetsum= (n*(n-1))/2;
         cout<<targetsum;
 
         int sum=0;
         for(int i=0;i<n-1;i++){
             sum+=nums[i];
-
         }
-        cout<<sum;
-        if(targetsum==sum) return 0;
 
-        return (targetsum-sum);
-        
+        if(targetsum==sum) return 0;
+        return (targetsum-sum);        
     }
 };
