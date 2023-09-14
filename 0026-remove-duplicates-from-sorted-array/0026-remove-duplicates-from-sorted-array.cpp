c60fa6d300749f4adb11f80ba53ca5f8c46862f5
Bhavya Srivastava <83921370+N-epiphany@users.noreply.github.com>
2023-09-15 01:05:58 +0530

    Time: 66 ms (7.48%), Space: 18.9 MB (12.5%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        // sort(nums.begin(),nums.end());
+        int first , second;
+         
+        for(int first=0 , second =1;second<nums.size();first++,second++){
+            cout<<first<<":"<<second<<endl;
+            if(nums[first]!=nums[second]) continue;
+            else{
+             nums[first]=-102;   
+            }
+            cout<<nums[first];
+        }
+
+        
+        vector<int> temp;
+        int j=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==-102) continue;
+            else {
+                temp.push_back(nums[i]);
+                j++;
+            }
+        }
+        nums=temp;
+        for(int i=0;i<nums.size();i++){
+            cout<<temp[i];
+        }
+        return temp.size();
+    }
+};
