054b46010d5d2ddde4c6674ef5bfe28116cb8943
Bhavya Srivastava <83921370+N-epiphany@users.noreply.github.com>
2023-10-08 19:19:45 +0530

    Time: 15 ms (50.98%), Space: 20 MB (10.12%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,27 +1,28 @@
 class Solution {
    public: int majorityElement(vector < int > & nums) {
-      int n = nums.size();
-      int majority = n / 2;
-      sort(nums.begin(), nums.end());
-      int num = nums[0];
-      int count = 0;
+            //method 1 time complexity O(nlogn)
+            int n = nums.size();
+            int majority = n / 2;
+            sort(nums.begin(), nums.end());
+            int num = nums[0];
+            int count = 0;
 
-    //   cout << "initial values are \n majority :" << majority << "\n num:" << num << "\n count:" << count;
-      for (int i = 0; i < n; i++) {
-        //  cout << "num" << num << ":";
-        //  cout << "nums" << nums[i] << endl;
-         if (nums[i] == num) {
-            count++;
-            // cout << "element found" << num;
-         } else {
-            num = nums[i];
-            count = 1;
-         }
-         if (count > majority) {
-            return num;
-         }
-      }
-      return -1;
+            //   cout << "initial values are \n majority :" << majority << "\n num:" << num << "\n count:" << count;
+            for (int i = 0; i < n; i++) {
+                //  cout << "num" << num << ":";
+                //  cout << "nums" << nums[i] << endl;
+                if (nums[i] == num) {
+                count++;
+                // cout << "element found" << num;
+                } else {
+                num = nums[i];
+                count = 1;
+                }
+                if (count > majority) {
+                return num;
+                }
+            }
+            return -1;
 
    }
 };
